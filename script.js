b4b1b62f6429eb278327c78a66b0a2a3b2b3cf2e
Gus Stoermer <gstoermer1@gmail.com>
2026-07-17 15:22:14 -0400

    Add random text shadow

--- script.js
+++ script.js
@@ -6,3 +6,17 @@ window.onload = () => {
 
 window.addEventListener("resize", stars.event_resize);
 window.addEventListener("pointermove", stars.event_mousemove);
+
+window.addEventListener("DOMContentLoaded", () => {
+  const classes = [
+    "text-shadow-p1",
+    "text-shadow-p2",
+    "text-shadow-a1",
+    "text-shadow-a2"
+  ];
+
+  const randomClass = classes[Math.floor(Math.random() * classes.length)];
+  [...document.getElementsByClassName("text-shadow-random")].forEach(e => {
+    e.classList.add(randomClass);
+  })
+});
