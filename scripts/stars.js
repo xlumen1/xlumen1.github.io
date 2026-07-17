02a1d67affe281d2ba83eef3e4a30b6e2392ee68
Gus Stoermer <gstoermer1@gmail.com>
2026-07-17 15:29:43 -0400

    Add dynamic star count choosing

--- scripts/stars.js
+++ scripts/stars.js
@@ -41,7 +41,7 @@ function U_VectorAverage(v) {
 var D_Canvas_Stardata = {
   star_radius: 2,
   stars: [],
-  count: 5000,
+  count: Math.floor(window.innerWidth * window.innerHeight / 350),
 };
 
 // mouse state (normalized 0..1)
