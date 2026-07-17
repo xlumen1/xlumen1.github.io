3d06046d74d5aa2c9edc0c088a29feedc74654e5
Gus Stoermer <gstoemer1@gmail.com>
2026-07-17 09:50:38 -0400

    Switch to pointer events

--- script.js
+++ script.js
@@ -5,6 +5,4 @@ window.onload = () => {
 };
 
 window.addEventListener("resize", stars.event_resize);
-window.addEventListener("mousemove", stars.event_mousemove);
-window.addEventListener("mouseleave", stars.event_mouseleave);
-window.addEventListener("mouseenter", stars.event_mouseenter);
+window.addEventListener("pointermove", stars.event_mousemove);
